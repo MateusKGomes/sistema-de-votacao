f6b4b2f2308ed11b92713a16062c35c369398349
MateusKGomes <mateuskulik30@gmail.com>
2023-08-22 15:10:19 -0300

    Requisito 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public abstract class PessoaCandidata extends Pessoa {
+
+  int numero;
+  int votos;
+
+  PessoaCandidata(String nome, int numero) {
+    this.votos = 0;
+    this.numero = numero;
+    super.nome = nome;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 
 }
